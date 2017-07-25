6585ee962a79c48303c1443687714ac813d12219
Israel Or <israelor@Israels-MacBook-Air-5.local>
2017-07-25 21:46:46 +0300

    middle init.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,7 @@ var app = angular.module('starter.controllers', ["firebase"]).
 /**********************************
  * [CONTROLLER] WELCOME
  *********************************/
-app.controller('WelcomeCtrl', function(Firebase, Auth, $scope, $stateParams, $ionicModal, $location) {
+app.controller('WelcomeCtrl', function(Firebase, Auth, $scope, $stateParams, $ionicModal, $location, $rootScope) {
 
   $scope.error = null;
   $scope.register = false;
@@ -38,6 +38,7 @@ app.controller('WelcomeCtrl', function(Firebase, Auth, $scope, $stateParams, $io
     if (authData) {
       // Define signed user
       console.log("Signed in as :", authData.email);
+      $rootScope.userName = authData.email;
       $scope.loggedInUser = authData;
       // Close register modal in register case
       $scope.closeRegister();
@@ -136,18 +137,96 @@ app.controller('WelcomeCtrl', function(Firebase, Auth, $scope, $stateParams, $io
 /**********************************
  * [CONTROLLER] CAMERA
  *********************************/
-app.controller('playlistCtrl', function($scope, $stateParams, Database) {
+app.controller('playlistCtrl', function($scope, $stateParams, Database, $ionicModal, $http, $ionicPopup, $rootScope) {
 	$scope.roomName = $stateParams.roomName;
 
-	Database.ref('rooms').orderByChild('name').equalTo($scope.roomName).limitToFirst(1).on("value", function(snapshot) {
-		$scope.allSongs = snapshot.val()["2"].songs;
-		console.log(snapshot.val());
-		$scope.$apply();
+	Database.ref('rooms/' + $scope.roomName).on("value", function(snapshot) {
+		$scope.allSongs = snapshot.val().songs;
+		if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+
+			$scope.$apply();
+		}
 	});
 
 	$scope.addNewSong = function () {
-		
+		$ionicModal.fromTemplateUrl('templates/addSongModal.html', {
+			scope: $scope,
+			animation: 'slide-in-up',
+		}).then(function(modal) {
+			$scope.modalAddSong = modal;
+			$scope.modalAddSong.show();
+		});
+	};
+
+	$scope.addSongToDBList = function (song) {
+		$http.get('https://www.googleapis.com/youtube/v3/videos', {
+			params: {
+				key: "AIzaSyC-0pj2WMY25D3OzmB-Icl3GmBPI4PICjg",
+				part: 'snippet, contentDetails',
+				id: song.id.videoId
+			}
+		}).then(function (data) {
+			var duration = convertISO8601ToSeconds(data.data.items[0].contentDetails.duration) / 60;
+			Database.ref('rooms/' + $scope.roomName + '/songs').push({"length": duration, "name":song.snippet.title, "requester":$rootScope.userName || 'unknown', "thumb":song.snippet.thumbnails.default.url, "url":song.id.videoId}).then(function (data) {
+
+			}, function (err) {
+
+			});
+		}, function (err) {
+
+		});
+	};
+
+	function convertISO8601ToSeconds(input) {
+
+		var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+		var hours = 0, minutes = 0, seconds = 0, totalseconds;
+
+		if (reptms.test(input)) {
+			var matches = reptms.exec(input);
+			if (matches[1]) hours = Number(matches[1]);
+			if (matches[2]) minutes = Number(matches[2]);
+			if (matches[3]) seconds = Number(matches[3]);
+			totalseconds = hours * 3600  + minutes * 60 + seconds;
+		}
+
+		return (totalseconds);
 	}
+
+	$scope.addSongToList = function (song) {
+		var myPopup = $ionicPopup.show({
+			template: '<p style="direction: rtl">להוסיף שיר לרשימה?</p>',
+			scope: $scope,
+			buttons: [
+				{ text: 'בטל', type:'button-calm' },
+				{
+					text: '<b>הוסף</b>',
+					type: 'button-positive',
+					onTap: function(e) {
+						$scope.addSongToDBList(song);
+					}
+				}
+			]
+		});
+	};
+
+	$scope.getYoutubeData = function(searchText){
+		$http.get('https://www.googleapis.com/youtube/v3/search', {
+			params: {
+				key: "AIzaSyC-0pj2WMY25D3OzmB-Icl3GmBPI4PICjg",
+				type: 'audio',
+				maxResults: '12',
+				pageToken: $scope.nextPage ? $scope.nextPage : '',
+				part: 'id,snippet',
+				fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle,nextPageToken,prevPageToken',
+				q: searchText
+			}
+		}).then(function (data) {
+			$scope.suggestions = data.data.items;
+		}, function (err) {
+
+		})
+	};
 });
 
 /**********************************
@@ -156,8 +235,8 @@ app.controller('playlistCtrl', function($scope, $stateParams, Database) {
 app.controller('SearchCtrl', function($scope, roomsService, Database, $ionicPopup, $state) {
 
 		$scope.logInToRoom = function (roomName, pin) {
-			Database.ref('rooms').orderByChild('name').equalTo(roomName).limitToFirst(1).on("value", function(snapshot) {
-				if(snapshot.val() && pin == snapshot.val()["2"].pin) {
+			Database.ref('rooms/' + roomName).on("value", function(snapshot) {
+				if(snapshot.val() && pin == snapshot.val().pin) {
 					$state.go('playlist', {roomName: roomName})
 				} else {
 					var myPopup = $ionicPopup.show({
