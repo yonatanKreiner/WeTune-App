131080991bab5783a370aa947060d44ed558f3ac
Israel Or <israelor@Israels-MacBook-Air-5.local>
2017-07-27 19:08:09 +0300

    fix convertISO8601ToSeconds function to support mobile devices.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -284,21 +284,18 @@ app.controller('playlistCtrl', function($scope, $stateParams, Database, Auth, $i
 
 	function convertISO8601ToSeconds(input) {
 
-		var d = new Date(input);
+		var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+		var hours = 0, minutes = 0, seconds = 0, totalseconds;
 
-		// var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
-		// var hours = 0, minutes = 0, seconds = 0, totalseconds;
+		if (reptms.test(input)) {
+			var matches = reptms.exec(input);
+			if (matches[1]) hours = Number(matches[1]);
+			if (matches[2]) minutes = Number(matches[2]);
+			if (matches[3]) seconds = Number(matches[3]);
+			totalseconds = hours * 3600  + minutes * 60 + seconds;
+		}
 
-		// if (reptms.test(input)) {
-		// 	var matches = reptms.exec(input);
-		// 	if (matches[1]) hours = Number(matches[1]);
-		// 	if (matches[2]) minutes = Number(matches[2]);
-		// 	if (matches[3]) seconds = Number(matches[3]);
-		// 	totalseconds = hours * 3600  + minutes * 60 + seconds;
-		// }
-
-		console.log(d);d
-		return (d.getSeconds());
+		return (totalseconds);
 	}
 
 	$scope.getYoutubeData = function(searchText){
